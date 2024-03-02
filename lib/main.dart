a94d5382b014efba2a252b3eb75fbb44796054de
hafsaabidsaeed <128362965+hafsaabidsaeed@users.noreply.github.com>
2024-03-03 03:22:41 +0500

    UI

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Seller Application',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.green),
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.purple),
         useMaterial3: true,
       ),
     );
