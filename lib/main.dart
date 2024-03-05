80581e3bb570cd584e6b32bf4cc28f54fbeb963b
hafsaabidsaeed <128362965+hafsaabidsaeed@users.noreply.github.com>
2024-03-05 10:37:02 +0500

    App

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Seller Application',
+      title: 'Seller ',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.purple),
         useMaterial3: true,
